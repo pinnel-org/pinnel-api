2e61fc3ca471fa8bbe7efb9299aee2bd962292aa
shaitan <shaitanaleksender@gmail.com>
2026-05-04 10:32:33 +0200

    Initialize Maven-based Spring Boot project with basic structure and dependencies for `pinnel-api`.

---
+++ src/main/java/org/pinnel/pinnelapi/PinnelApi1Application.java
@@ -0,0 +1,13 @@
+package org.pinnel.pinnelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinnelApi1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinnelApi1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/pinnel/pinnelapi/PinnelApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pinnel.pinnelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinnelApi1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
